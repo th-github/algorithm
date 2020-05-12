2ee341ba5a2091b6f783ebf94cf9cb9950a8d0cf
Tom4FirmwareEng <w364th@gmail.com>
2020-05-12 09:33:13 -0700

    C structure data padding and packing
    C structure data padding and packing

--- padandpack.c
+++ padandpack.c
@@ -8,9 +8,7 @@ padandpack.c
 void fp(void);
 
 int main(void)
-{
-    int v = 0;
-    
+{  
     struct type_st1 {
         char c;     // 2 bytes,  padded bytes to align to type long
         long l;     // 8 bytes  full width
@@ -25,20 +23,17 @@ int main(void)
         double l;     // 8 bytes  full width
         short  s;     // 2 bytes    packed with the next shorter one
         char c;       // 2 bytes    packed with the former longer one
-    };
-    
+    };   
     struct type_st4 {
         int  i;     // 4 bytes
         float l;    // 4 bytes  
         short s;    // 2 bytes   padded to type float
     };
-
     struct type_st5 {
         long l;     // 8 bytes  full width
         short s;    // 2 bytes   padded to type int
         int  i;     // 4 bytes   
     };
-
     struct type_st6 {   // declare in increasing order
         int  i;       // 4 bytes   padded to type long
         double l;     // 8 bytes  full width
@@ -50,35 +45,15 @@ int main(void)
         char c; // padded to type int 
     };
                         
-    struct type_st1   st1;
-    v = sizeof(st1);
-    printf("sizeof struct st1: %d\n", v); 
-    
-    struct type_st2   st2;
-    v = sizeof(st2);
-    printf("sizeof struct st2: %d\n", v);    
-         
-    struct type_st3   st3;
-    v = sizeof(st3);
-    printf("sizeof struct st3: %d\n", v);
+    printf("sizeof struct st1: %d\n", (int)sizeof(struct type_st1)); 
+    printf("sizeof struct st2: %d\n", (int)sizeof(struct type_st2));            
+    printf("sizeof struct st3: %d\n", (int)sizeof(struct type_st3));
+    printf("sizeof struct st4: %d\n", (int)sizeof(struct type_st4));
+    printf("sizeof struct st5: %d\n", (int)sizeof(struct type_st5));
+    printf("sizeof struct st6: %d\n", (int)sizeof(struct type_st6));
 
-    struct type_st4   st4;
-    v = sizeof(st4);
-    printf("sizeof struct st4: %d\n", v);
-
-    struct type_st5   st5;
-    v = sizeof(st5);
-    printf("sizeof struct st5: %d\n", v);
-
-    struct type_st6   st6;
-    v = sizeof(st6);
-    printf("sizeof struct st6: %d\n", v);
-
-    struct type_ft   ft;
-    v = sizeof(void*);
-    printf("sizeof void*: %d\n", v);
-    v = sizeof(ft);
-    printf("sizeof struct ft: %d\n", v);
+    printf("sizeof void*: %d\n", (int)sizeof(void*));
+    printf("sizeof struct ft: %d\n", (int)sizeof(struct type_ft));
                                   
     printf("\n"); 
 	return 0;
