6899fc43e75617fb56b0ce0f1398d590999ee560
Tom4FirmwareEng <w364th@gmail.com>
2022-07-10 20:34:43 -0700

    Update bits_test.c

--- bits_test.c
+++ bits_test.c
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <bitset>
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -17,14 +17,14 @@ int togglebits(int data);
 int rotatebits(int data);
 void test_findlsb_1bit(unsigned int data);
 
-int findlsb_0bit(int data);
+void findlsb_0bit(int data);
 int changeselectbits_16(int data, int v, int u, int l, bool toggle);
-void test_clearbits(unsigned int data, short u, short l);
-void test_setbits(unsigned int data, short u, short l);
+void test_clearbits(unsigned int data, int u, int l);
+void test_setbits(unsigned int data, int u, int l);
 void clr_lsb(unsigned int* data, int ith);
 void set_msb(unsigned int* data, int ith);
 unsigned short rotatebits(unsigned short);
-int dec2bin(unsigned int, unsigned int* bits);
+void dec2bin(unsigned int, unsigned int* bits);
 
 int main(void)
 {
@@ -66,7 +66,9 @@ int main(void)
     cout << endl;
     test_NumOposit(5, 9);
     cout << endl;
-        
+    test_NumOposit(7, 8);
+    cout << endl;
+	        
     test_findlsb_1bit(1);
     test_findlsb_1bit(255);
     test_findlsb_1bit(128);
@@ -76,30 +78,13 @@ int main(void)
     test_findlsb_1bit(0);            
     cout << endl;
     
-    val = 26;
-    cout << "lsb_0bit " << bitset<8>(val) << " " << "@ " 
-         << findlsb_0bit(val) << endl;
-
-    val = 29;
-    cout << "lsb_0bit " << bitset<8>(val) << " " << "@ " 
-         << findlsb_0bit(val) << endl;
-
-    val = 27;
-    cout << "lsb_0bit " << bitset<8>(val) << " " << "@ " 
-         << findlsb_0bit(val) << endl;
-         
-    cout << "lsb_0bit " << bitset<8>(0) << " " << "@ " 
-         << findlsb_0bit(0) << endl;
-         
-    val = 128;
-    cout << "lsb_0bit " << bitset<8>(val) << " " << "@ " 
-         << findlsb_0bit(val) << endl;
-
-    val = 255;
-    cout << "lsb_0bit " << bitset<8>(val) << " " << "@ " 
-         << findlsb_0bit(val) << endl;
+	findlsb_0bit(26);
+    findlsb_0bit(29);
+    findlsb_0bit(27);
+    findlsb_0bit(128);
+    findlsb_0bit(255);
     cout << endl;
-    
+	
     val = 29;
     cout << "val: " << bitset<8>(val) << endl;
     val = togglebits(val);
@@ -114,29 +99,32 @@ int main(void)
         
     cout << "change bits between[5,2] " << bitset<16>(val) << endl;
     data = changeselectbits_16(val, 3, 5,2, false); // toggle: false
-    cout << "change bits between[5,2] " << bitset<16>(data) << endl;
+    cout << "bits replaced " << bitset<16>(data) << endl;
     cout << endl;
 
     cout << "change bits between[5,2] " << bitset<16>(val) << endl;
     data = changeselectbits_16(val, 12, 5,2, false); // toggle: false
-    cout << "change bits between[5,2] " << bitset<16>(data) << endl;
+    cout << "bits replaced " << bitset<16>(data) << endl;
     cout << endl;
         
     n = 0xcafe;
     cout << "toggle bits between[10,3] " << bitset<16>(n) << endl;
     data = changeselectbits_16(n, 0, 10,3, true); // toggle: true
-    cout << "toggle bits between[10,3] " << bitset<16>(data) << endl;
+    cout <<  "bits toggled " << bitset<16>(data) << endl;
     cout << endl;
 
     cout << "toggle bits between[15,0] " << bitset<16>(n) << endl;
     data = changeselectbits_16(n, 0, 15,0, true); // toggle: true
-    cout << "toggle bits between[15,0] " << bitset<16>(data) << endl;
+    cout << "bits toggled " << bitset<16>(data) << endl;
     cout << endl;
                         
     test_clearbits(0xcafe, 10,3);
-    cout <<endl;
+    cout << endl;
+    test_clearbits(0xcafe, 10,0);
+    cout << endl;
     test_setbits(0xcafe, 10,3);
-    
+    cout << endl;
+    test_setbits(0xcafe, 10,0);
     cout << endl; 
     
     bitset<8*sizeof(short)> b;
@@ -160,6 +148,20 @@ int main(void)
      
     cout << "-----------" << endl; 
     return 0;
+}
+
+void dec2bin(unsigned int d, unsigned int* bits)
+{
+	int i = 31;
+	while (i--)
+		bits[i] = (d >> i) & 1;
+
+	i = 31;
+	do {
+		cout << bits[i];
+	} while (i--);
+	cout << endl;
+	
 }
 
 int b2d(int data)
@@ -187,7 +189,7 @@ int b2d(int data)
         v += (1 & (data >> 11))*2048;
         v += (1 & (data >> 12))*4096;
         v += (1 & (data >> 13))*8292;
-        v != v;
+        v = !v;
     } 
 
     return v;
@@ -234,7 +236,7 @@ void test_findlsb_1bit(unsigned int data)
 {
     int i = 1, v = data;
     if (v == 0) {
-        cout << "lsb_1bit " << bitset<8>(data) << " @ " << (i-1) << endl;
+        cout << "there is no lsb_1bit in " << bitset<8>(data) << endl;
         return;
     }
     
@@ -248,7 +250,7 @@ void test_findlsb_1bit(unsigned int data)
    }               
 }
 
-int findlsb_0bit(int data)
+void findlsb_0bit(int data)
 {
     int i = 1, v = data;
     while(v) {
@@ -256,10 +258,13 @@ int findlsb_0bit(int data)
             v = v >> 1;  
             i++;  
         }         
-        else
-            break;    
+        else {
+			cout << "lsb_0bit " << bitset<8>(data) << " @ " << (i-1) << endl;
+            break; 
+		}   
    }
-   return i-1;
+   if (i > 7)
+	   cout << "there is no lsb_0bit in " << bitset<8>(data) << endl;
 }
 
 int togglebits(int data)
@@ -272,7 +277,7 @@ int togglebits(int data)
         if (temp & 1)   len++;
         temp = (temp >> 1);
     }
-    mask = (1 << len+1) - 1;  // make a set-bit mask start from msb 1-bit
+    mask = (1 << (len+1)) - 1;  // make a set-bit mask start from msb 1-bit
     return data ^ mask;  
 
 }
@@ -314,41 +319,53 @@ int changeselectbits_16(int source, int v, int u, int l, bool toggle)
         cout <<  "toggle bits" << endl;  
         return  source ^ (~mask);
     }
-    cout << "insert " << v << " " << bitset<8>(source) << endl;
+    cout << "replace " << v << " " << bitset<8>(source) << endl;
     temp &= mask;
     v = v << l;
     
     return temp | v;
 }
 
-void test_clearbits(unsigned int data, short u, short l)
+void test_clearbits(unsigned int data, int u, int l)
 {
     unsigned int mask = 0;
-    
-    mask  = 0xFFFF << (u+1);
-    mask |= (0xFFFF >> (u+l+1));
-    cout << "mask clr: " << bitset<16>(mask) << endl;
+	unsigned int n = (short)(sizeof(unsigned int) * 8);
+
+    if (u >= n || l < 0) {
+		cout << "invalid range given" << endl;
+		return;
+	}
+
+	mask = 0xFFFFFFFF >> (n - u);
+	mask &= 0xFFFFFFFF << l;
+	mask = ~mask;
+    cout << "mask clr: " << bitset<32>(mask) << endl;
         
-    cout << "old data: " << bitset<16>(data) << endl;
-    cout << "new data: " << bitset<16>(data & mask) << endl;    
+    cout << "old data: " << bitset<32>(data) << endl;
+    cout << "new data: " << bitset<32>(data & mask) << endl;    
 }
 
-void test_setbits(unsigned int data, short u, short l)
+void test_setbits(unsigned int data, int u, int l)
 {
     unsigned int mask = 0;
-    
-    mask  = 0xFFFF  << (u+1);
-    mask |= 0xFFFF >> (u+l+1);
-    mask = ~mask;
-    cout << "mask set: " << bitset<16>(mask) << endl;
+	unsigned short n = (short)(sizeof(unsigned int) * 8);
+
+    if (u >= n || l < 0) {
+		cout << "invalid range given" << endl;
+		return;
+	}
+
+	mask = 0xFFFFFFFF >> (n - u);
+	mask &= 0xFFFFFFFF << l;	
+    cout << "mask set: " << bitset<32>(mask) << endl;
         
-    cout << "old data: " << bitset<16>(data) << endl;
-    cout << "new data: " << bitset<16>(data | mask) << endl;    
+    cout << "old data: " << bitset<32>(data) << endl;
+    cout << "new data: " << bitset<32>(data | mask) << endl;    
 }
 
 void clr_lsb(unsigned int* data, int ith)
 {
-    unsigned int m = (~0 << ith+1);   // ie., 0x...FFF0000...
+    unsigned int m = (~0 << (ith+1));   // ie., 0xFF...FFF0000
     *data &= m;
 }
 
@@ -388,17 +405,104 @@ int countbits(unsigned int data)
     return n;
 }
 
-int dec2bin(unsigned int d, unsigned int* bits)
-{
-	int i = 31;
-	while (i--)
-		bits[i] = (d >> i) & 1;
 
-	i = 31;
-	do {
-		cout << bits[i];
-	} while (i--);
-	cout << endl;
-}
+/*********** output **********
+Admins-MacBook-Pro:algorithm-master admin$ ./a.out
+00000000000000000000000100011010
+
+bit counts in data: 4
+
+ binary: 0000000100011010 to integer: 0
+
+msb_1bit in: 0000000100011010 is at: 4th position
+
+test alternate pattern of: 0
+
+test alternate pattern of: 1
+
+swap two bits [2,6] 
+ in: 0000000000011100
+new: 0000000001011000
+
+swap two bits [2,6] 
+ in: 0000000000101011
+new: 0000000000101011
+
+swap two bits [2,6] 
+ in: 0000000000000111
+new: 0000000001000011
+
+10,-10 is oposit? 1
+
+5,9 is oposit? 0
+
+7,8 is oposit? 0
+
+lsb_1bit 00000001 @ 0
+lsb_1bit 11111111 @ 0
+lsb_1bit 10000000 @ 7
+lsb_1bit 00011101 @ 0
+lsb_1bit 00011100 @ 2
+lsb_1bit 00011010 @ 1
+there is no lsb_1bit in 00000000
+
+lsb_0bit 00011010 @ 0
+lsb_0bit 00011101 @ 1
+lsb_0bit 00011011 @ 2
+lsb_0bit 10000000 @ 0
+there is no lsb_0bit in 11111111
+
+val: 00011101
+toggled: 00000010
+
+val: 00011101
+rotatebits: 00010111
+
+change bits between[5,2] 0000000000010111
+mask: 1111111111000011  replace 3 00010111
+bits replaced 0000000000001111
+
+change bits between[5,2] 0000000000010111
+mask: 1111111111000011  replace 12 00010111
+bits replaced 0000000000110011
+
+toggle bits between[10,3] 1100101011111110
+mask: 1111100000000111  toggle bits
+bits toggled 1100110100000110
+
+toggle bits between[15,0] 1100101011111110
+mask: 0000000000000000  toggle bits
+bits toggled 0011010100000001
+
+mask clr: 11111111111111111111110000000111
+old data: 00000000000000001100101011111110
+new data: 00000000000000001100100000000110
+
+mask clr: 11111111111111111111110000000000
+old data: 00000000000000001100101011111110
+new data: 00000000000000001100100000000000
+
+mask set: 00000000000000000000001111111000
+old data: 00000000000000001100101011111110
+new data: 00000000000000001100101111111110
+
+mask set: 00000000000000000000001111111111
+old data: 00000000000000001100101011111110
+new data: 00000000000000001100101111111111
+
+b: 1100101011111110
+b: 1 0
+data: 1000111101111001
+clear all lsb from bit 6
+data: 1000111100000000
+
+set all msb 00100000 from bit 6
+new result: 11100000
+set all msb 00010000 from bit 1
+new result: 11111110
+
+-----------
+
+*************/
 
 
